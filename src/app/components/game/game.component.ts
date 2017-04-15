e2bed5eacbc5cee875d66b271cbf6c35f6b0a3e2
刘忍 <18826073587@163.com>
2017-04-15 15:53:35 +0800

    着手游戏页面的题

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.sass']
 })
 export class GameComponent implements OnInit {
 
